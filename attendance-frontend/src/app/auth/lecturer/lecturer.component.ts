d44958ccf2f98e8ef266406460c480da9f1c22d1
sivakumar6678 <chandragarisivakumar@gmail.com>
2025-01-08 23:03:57 +0530

    add admin, lecturer, student, and home components with basic templates and tests

---
+++ attendance-frontend/src/app/auth/lecturer/lecturer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lecturer',
+  standalone: true,
+  imports: [],
+  templateUrl: './lecturer.component.html',
+  styleUrl: './lecturer.component.css'
+})
+export class LecturerComponent {
+
+}
