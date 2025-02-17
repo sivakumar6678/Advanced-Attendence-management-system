fee35f96400d1f382f941ca1e5192b59ecd9f971
sivakumar6678 <chandragarisivakumar@gmail.com>
2025-02-17 14:53:40 +0530

    added some admin changes

--- attendance-backend/teacher/admin.py
+++ attendance-backend/teacher/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Faculty
 # Register your models here.
+@admin.register(Faculty)
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'email', 'branch', 'is_active','password','phone_number','employee_id','joined_date','exit_date','rejoin_date','is_admin')
+    search_fields = ('full_name', 'email', 'branch')
+    list_filter = ('branch', 'is_active')
+    ordering = ('branch', 'full_name')
