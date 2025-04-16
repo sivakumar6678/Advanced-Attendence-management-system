aedd6dd1461f2fb9faaa25db40381d4933001a20
sivakumar6678 <chandragarisivakumar@gmail.com>
2025-04-16 18:29:04 +0530

    feat: configure MySQL database settings in settings.py

--- attendance-backend/attendance_system/settings.py
+++ attendance-backend/attendance_system/settings.py
@@ -113,14 +113,14 @@ DATABASES = {
     #     'HOST' : 'localhost',
     #     'PORT' : '3306'
     # }
-#  'default': {
-#     #     'ENGINE': 'django.db.backends.mysql',
-#     #     'NAME': os.getenv('DB_NAME'),
-#     #     'USER': os.getenv('DB_USER'),
-#     #     'PASSWORD': os.getenv('DB_PASSWORD'),
-#     #     'HOST': os.getenv('DB_HOST'),
-#     #     'PORT': os.getenv('DB_PORT', '3306'),
-#     # }   # 
+ 'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+     }   
 }
 
 
