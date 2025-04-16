a2b5b493875dd256f2878146d23ec6e576c217fa
sivakumar6678 <chandragarisivakumar@gmail.com>
2025-04-16 15:32:01 +0530

    fix

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python attendance-backend/manage.py migrate --noinput
 python attendance-backend/manage.py collectstatic --noinput
-gunicorn attendance-backend.attendance_system.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn attendance_system.wsgi:application --bind 0.0.0.0:$PORT
